30d858e2a3f75b2499be416cc919e12550644155
martin-han26 <sixh0626@163.com>
2023-07-12 21:10:10 +1000

    Add files via upload

---
+++ Simulate data modelling.R
@@ -0,0 +1,17 @@
+library(dlnm)
+library(mgcv)
+library(ggplot2)
+
+
+data("chicagoNMMAPS")
+cbCHIpm =crossbasis(chicagoNMMAPS$temp, lag = 5, argvar = list(fun="ns", 3), 
+                    arglag = list(fun='ns',6))
+CHIglm=glm(death~cbCHIpm:o3,family = quasipoisson(), data = chicagoNMMAPS)
+CHIpre=crosspred(cbCHIpm,CHIglm)
+plot(CHIpre,ticktype='detailed',xlab="\n\n PM 2.5",ylab="\n\n Lag (weeks)", zlab="\n\n RR",
+     border='#3366FF',
+     # col='#99FFCC',
+     shade = 0.1,
+     cex.lab=1.3,cex.axis=1.2,lwd=1.3,
+     theta = 40, phi = 28, # theta for left-right, phi for up-down
+     ltheta = 35)
